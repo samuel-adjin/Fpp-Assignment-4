b8e132b35722f2acfc1de422e177ae7d0d4ea304
Vandeth <vandeth.ifl@gmail.com>
2024-12-03 21:04:37 -0600

    Update the output for problem 4

--- src/problemfour/TestClass.java
+++ src/problemfour/TestClass.java
@@ -1,7 +1,7 @@
 package problemfour;
 
 public class TestClass {
-    public static Employee getMaxSalary(Employee[] emp) {
+    public static Employee findMaxSalary(Employee[] emp) {
         if (emp == null || emp.length == 0) {
             return null;
         }
@@ -16,18 +16,16 @@ public class TestClass {
 
     public static void main(String[] args) {
     CommissionEmployee ce = new CommissionEmployee("Karen", "Jones","SSN0001",6000,0.30);
-    BasePlusCommissionEmployee base = new BasePlusCommissionEmployee("Donald","Trump","SSN0002",10000,.3,500);
+    BasePlusCommissionEmployee base1 = new BasePlusCommissionEmployee("Donald","Sam","SSN0002",1000,.3,500);
+    BasePlusCommissionEmployee base2 = new BasePlusCommissionEmployee("Bill","Tim","SSN0005",18000,.3,1000);
     HourlyEmployee he = new HourlyEmployee("Houston","Jimmy","SSN0003",23,20);
     SalariedEmployee se = new SalariedEmployee("Jonh","Jon","SSN0004",2000);
-    Employee[] emp = new Employee[]{ce,base,he,se};
+    Employee[] emp = new Employee[]{ce,base1,base2,he,se};
     for(Employee em: emp){
         System.out.println(em);
     }
 
-    Employee maxSalaryEmployee = getMaxSalary(emp);
-    for (Employee em : emp) {
-        System.out.println(em);
-    }
+    Employee maxSalaryEmployee = findMaxSalary(emp);
     if (maxSalaryEmployee != null) {
         System.out.printf("The employee with the maximum salary is: %s %s with a salary of %.2f%n", maxSalaryEmployee.getFirstName(), maxSalaryEmployee.getLastName(), maxSalaryEmployee.getPayment());
     }
